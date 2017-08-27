c650f2db9c9721c7f7449b1a75443e47368b8e6c
Richard Chang <chiel99@gmail.com>
2017-08-28 02:49:13 +0800

    [Title]: Implement minimal requirement to pass unit test, Status: GREEN
    [Problem]:
    [Solution]:
    [Note]:

--- app/src/main/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCart.java
+++ app/src/main/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCart.java
@@ -2,11 +2,18 @@ package io.twincatstudio.tdd_daytwohomework;
 
 
 class PotterShoppingCart {
+    private int[] mBooks = new int[5];
 
     public void addBooks(int series, int number) {
+        mBooks[series-1] += number;
     }
 
     public int getTotalPrice() {
-        return 0;
+        int price = 0;
+
+        for (int i = 0 ; i < mBooks.length ; i++) {
+            price += mBooks[i] * 100;
+        }
+        return price;
     }
 }
