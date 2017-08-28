21e736813390a979e01cc0d6e863a066da43bd32
Richard Chang <chiel99@gmail.com>
2017-08-29 02:05:37 +0800

    [Title]: Add test case 2, Status: RED
    [Problem]:
    [Solution]:
    [Note]:

--- app/src/test/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCartUnitTest.java
+++ app/src/test/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCartUnitTest.java
@@ -7,11 +7,26 @@ import static org.junit.Assert.*;
 public class PotterShoppingCartUnitTest {
 
     @Test
-    public void getTotalPrice_chapter1_1_should_return_100() {
+    public void getTotalPrice_book1_1_should_return_100() throws Exception {
         // Arrange
         PotterShoppingCart target = new PotterShoppingCart();
         target.addBooks(1, 1);
         int expected = 100;
+
+        // Act
+        int actual = target.getTotalPrice();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getTotalPrice_book1_1_book2_1_should_return_190() throws Exception {
+        // Arrange
+        PotterShoppingCart target = new PotterShoppingCart();
+        target.addBooks(1, 1);
+        target.addBooks(2, 1);
+        int expected = 190;
 
         // Act
         int actual = target.getTotalPrice();
