e56c0cdb3e3f90d9d5df213aa84cc807b06ba820
Richard Chang <chiel99@gmail.com>
2017-08-30 01:27:30 +0800

    [Title]: Add 3rd test case, Status: RED
    [Problem]:
    [Solution]:
    [Note]:

--- app/src/test/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCartUnitTest.java
+++ app/src/test/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCartUnitTest.java
@@ -34,4 +34,20 @@ public class PotterShoppingCartUnitTest {
         // Assert
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void getTotalPrice_book1_1_book2_1_book3_1_should_return_270() throws Exception {
+        // Arrange
+        PotterShoppingCart target = new PotterShoppingCart();
+        target.addBooks(1, 1);
+        target.addBooks(2, 1);
+        target.addBooks(3, 1);
+        int expected = 270;
+
+        // Act
+        int actual = target.getTotalPrice();
+
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
