a6abc931692a5c6229d92cbf0a8404681f1a3e70
Richard Chang <chiel99@gmail.com>
2017-08-30 01:43:30 +0800

    [Title]: Implement for test case 4. Status: GREEN
    [Problem]:
    [Solution]:
    [Note]:

--- app/src/main/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCart.java
+++ app/src/main/java/io/twincatstudio/tdd_daytwohomework/PotterShoppingCart.java
@@ -36,6 +36,8 @@ class PotterShoppingCart {
                 return 0.95;
             case 3:
                 return 0.9;
+            case 4:
+                return 0.8;
             default:
                 return 1.0;
         }
